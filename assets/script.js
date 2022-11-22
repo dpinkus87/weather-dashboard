ae7c457c2b148d5229c825283fe0697feb82930a
dpinkus87 <dylanpinkus@gmail.com>
2022-11-22 14:21:22 -0600

    Finalized bootstrap layout

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,8 @@ var lat;
 var lon;
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-var forecastWeatherQueryURL = api.openweathermap.org/data/2.5/forecast? + lat + "&lon=" + lon + "&appid=" + apiKey;
-var reverseGeoCode = "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat + "&lon=" + lon + "&limit=&appid=" + apiKey;
+var forecastWeatherQueryURL = "api.openweathermap.org/data/2.5/forecast?" + lat + "&lon=" + lon + "&appid=" + apiKey;
+var reverseGeoCode = "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat + "&lon=" + lon + "&limit=" + "10" + "&appid=" + apiKey;
 
 function getAPI () {
     fetch(forecastWeatherQueryURL)
