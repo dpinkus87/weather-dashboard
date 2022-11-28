95046624c9d4a0b6b95366b5750a808d21bf64b4
dpinkus87 <dylanpinkus@gmail.com>
2022-11-28 15:59:48 -0600

    added for loop, updated forecast

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,97 @@
 // Global variables
 
 var apiKey = "70dc876db5069b24ab66ab771e564f0e"
-var cityName;
-var lat;
-var lon;
-var currentCity = $("#currentCity")
+
+
+// Current Weather variables
+var currentCity = $("#currentCity");
+var currentTemp = $("#humidity");
+var currentHumidity = $("#humidity");
+var currentWind = $("#wind");
+var cityArray = [];
+
 var searchHistory = $(".searchHistory")
 
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
-var forecastWeatherQueryURL = "api.openweathermap.org/data/2.5/forecast?" + lat + "&lon=" + lon + "&appid=" + apiKey;
-
-var reverseGeoCode = "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat + "&lon=" + lon + "&limit=" + "10" + "&appid=" + apiKey;
-
-var geocode = "http://api.openweathermap.org/geo/1.0/direct?q=" {city name} "& cityname=", {state code},{country code} "&appid=" + apiKey
 
 
 // Function to fetch weather API
-function getAPI () {
+function getCity() {
+    var cityName = $(".form-control").val()
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
+    fetch(queryURL)
+        .then(function (response) {
+            console.log(response)
+            if (response.ok) {
+                return response.json()
+            }
+        }).then(data => {
+            var city = {
+                name: data.name,
+                lat: data.coord.lat,
+                lon: data.coord.lon,
+            }
+            getForecast(city)
+            displayCurrentweather(data)
+        })
+
+}
+
+function getForecast(city) {
+    var forecastWeatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + city.lat + "&lon=" + city.lon + "&units=imperial&appid=" + apiKey;
     fetch(forecastWeatherQueryURL)
-    .then(function (response) {
-        return response.json()
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+        }).then(data => {
+            console.log(data);
+
+            displayForecast(data)
+
+        })
+}
+
+function displayCurrentweather(data) {
+    $("#current-city").text(data.name)
+    $("#temp").text("Temp: " + data.main.temp + " (F)")
+    $("#wind").text("Wind speed: " + data.wind.speed + "mph")
+    $("#humidity").text("Humidity: " + data.main.humidity + "%")
+
+    console.log(data);
+
+}
+
+function displayForecast(data) {
+    for(i = 0, i < 5; i++;){
+        var fDate = data.list[0].dt_txt;
+        var fImg = "https://openweathermap.org/img/wn/" + iconcode + ".png"
+        var fTemp = data.list[0].main.temp + " (F)";
+        var fWind = data.list[0].wind.speed + "mph";
+        var fHumidity = data.list[0].main.humidity + "%";
+
+        $("#fDate" + i).html(fDate)
+        $("#fImg" + i).html("<img src="+fImg+">");
+        $("#fTemp" + i).html(fTemp+"&#8457");
+        $("#fWInd" + i).html(fWind+"MPH");
+        $("#fHumidity" + i).html(fHumidity+"%");
     }
-    )
-} 
+}
+
+// Create list of cities previously searched
+function cityList(data) {
+    var listEl = $("<li> + data.toUpperCase() + </li>");
+    $(listEl).attr("class", "list-group-item");
+    $(listEl).attr("data-value", data.toUpperCase());
+    $(".searchHistory").append(listEl)
+    console.log(cityList);
+}
+
+// function previousCities() {
+//     var liEL = ""
+// }
+
+
+$(".btn-primary").on('click', getCity)
+// $(document).on('click', previousCities)
